8771c924da70bb3e1971aec4c951c843598f3ec0
Aaryan Sahu <sahuaryan1980@gmail.com>
2025-04-27 19:52:41 +0530

    Improve Vite configuration to support both development and production modes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Changed to root path for local development
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/English-Learning-App/' : '/',
+  }
 })
