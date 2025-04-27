58e1666ac06d3c826a6bceee6c37dbf0b8aab060
Aaryan Sahu <sahuaryan1980@gmail.com>
2025-04-27 19:59:13 +0530

    Update configurations for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? '/English-Learning-App/' : '/',
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/' // Use root path for Vercel deployment
 })
